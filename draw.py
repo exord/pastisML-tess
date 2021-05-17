d2380118011d54edfc58855a10a120b49905b307
Rodrigo F. Díaz <13200354+exord@users.noreply.github.com>
2021-05-17 11:46:25 -0300

    Use limit in radius taken from paramaters file

--- draw.py
+++ draw.py
@@ -13,11 +13,12 @@ import os
 # import pandas as pd
 
 import core as c
+import parameters as p
 
 homedir = os.getenv('HOME')
 mldir = os.path.join(homedir, 'EXOML', 'TESS-pastis')
 
-def draw_parameters(params, scenario, nsimu=1):
+def draw_parameters(params, scenario, nsimu=1, **kwargs):
     """
     Draw the parameters for a set of target (TIC) star.
     
@@ -30,13 +31,15 @@ def draw_parameters(params, scenario, nsimu=1):
     :param str scenario: defines the scenario for which to draw parameters
     
     :param int nsimu: size of parameter set to draw
+    
+    The remaining kwargs are passed to the _draw_parameters functions
     """
     ticstar = c.TargetStarParameters(params)
     ticstar.draw()
     
     # Draw parameters and return input dict for pastis
     if scenario in ['PLA', 'planet']:
-        pla_params = _draw_parameters_pla(ticstar)
+        pla_params = _draw_parameters_pla(ticstar, **kwargs)
         
         # Construct planet dict for pastis        
         # planetdict = pla_params[-1].to_pastis()
@@ -49,7 +52,7 @@ def draw_parameters(params, scenario, nsimu=1):
                       'PlanSys1': planetdict}        
         
     elif scenario in ['BEB', 'beb']:
-        beb_params = _draw_parameters_beb(ticstar)
+        beb_params = _draw_parameters_beb(ticstar, **kwargs)
         
         # Construct binary dict for pastis
         binarydict = beb_params[-1].to_pastis()
@@ -66,17 +69,18 @@ def draw_parameters(params, scenario, nsimu=1):
     return input_dict
 
 
-def _draw_parameters_pla(ticstar):
+def _draw_parameters_pla(ticstar, **kwargs):
     """Draw parameters for the Planet scenario."""
+    minimum_radius = kwargs.pop('minradius', p.MIN_PLA_RADIUS)
     # Instatiate planetary parameters
-    planet = c.PlanetParameters()
+    planet = c.PlanetParameters(minradius=minimum_radius)
     # Draw parameters
     planet.draw(len(ticstar))
         
     return planet
     
 
-def _draw_parameters_beb(ticstar):
+def _draw_parameters_beb(ticstar, **kwargs):
     """
     Draw parameters for the BEB scenario.
     
--- parameters.py
+++ parameters.py
@@ -1,12 +1,17 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 """
+Define parameters for simulations.
+
 Created on Fri May  7 17:34:03 2021
 
 @author: rodrigo
 """
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 SCENARIO = 'PLA'
 NSIMU_PER_TIC_STAR = 1
 
 # Directory with tables
 TABLE_DIR = 'tables/'
+
+# Limits on radius
+MIN_PLA_RADIUS = 1.0
