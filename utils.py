4f9e0b0d5a981236e585b41f055378080bf56bb8
Rodrigo F. Díaz <13200354+exord@users.noreply.github.com>
2021-07-26 14:56:23 -0300

    Refactor

--- utils.py
+++ utils.py
@@ -5,18 +5,19 @@ Created on Fri May 28 17:58:22 2021
 
 @author: rodrigo
 """
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from math import pi
 import numpy as np
 
-import constants as cts
+from . import constants as cts
+
 
 def sma(per, m1, m2):
     """
     Compute semi-major axis of relative orbit binary system.
-    
-    Primary star has mass m1[msun] and secondary has m2[msun] 
+
+    Primary star has mass m1[msun] and secondary has m2[msun]
     and period per [days].
     """
     per_s = per * 24 * 3600
@@ -26,9 +27,9 @@ def sma(per, m1, m2):
 def r_infconj(ecc, omega_deg, ar):
     """
     Compute separation at the time of inferior conjunction (transit).
-    
+
     Return value normalised to stellar radius
-    
+
     :param float or np.array ecc: orbital eccentricity
     :param float or np.array omega_deg: argument of pericenter (in deg)
     :param float or np.array ar: semi-major axis of orbit in units of stellar
@@ -36,4 +37,3 @@ def r_infconj(ecc, omega_deg, ar):
     """
     omega_rad = omega_deg * pi / 180.0
     return ar * (1 - ecc**2) / (1 + ecc * np.cos(pi/2 - omega_rad))
-
