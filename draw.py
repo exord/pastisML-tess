375c54db04f8c9b9045e602cc08af44b39a55469
Rodrigo F. Díaz <13200354+exord@users.noreply.github.com>
2021-07-26 11:19:24 -0300

    Refactoring+gitignore

--- draw.py
+++ draw.py
@@ -19,43 +19,44 @@ import utils as u
 homedir = os.getenv('HOME')
 mldir = os.path.join(homedir, 'EXOML', 'TESS-pastis')
 
+
 def draw_parameters(params, scenario, nsimu=1, **kwargs):
     """
     Draw the parameters for a set of target (TIC) star.
-    
+
     The draw is conditional on the parameter sets, representing the TIC star.
-    
-    :param iterable params: shape (nparams, size), list of parameters of TIC 
-    star. Is the second dimension is > 1, then nsimu draws are performed for 
+
+    :param iterable params: shape (nparams, size), list of parameters of TIC
+    star. Is the second dimension is > 1, then nsimu draws are performed for
     each parameter set.
-    
+
     :param str scenario: defines the scenario for which to draw parameters
-    
+
     :param int nsimu: size of parameter set to draw
-    
+
     The remaining kwargs are passed to the _draw_parameters functions
     """
     ticstar = c.TargetStarParameters(params)
     ticstar.draw()
-    
+
     # Draw parameters and return input dict for pastis
     if scenario in ['PLA', 'planet']:
         planet = _draw_parameters_pla(ticstar, **kwargs)
-        
+
         # Flag non-transiting planets
         flag_transit = conservative_transit_flag([ticstar, planet])
-        
-        # Construct planet dict for pastis        
+
+        # Construct planet dict for pastis
         # Add tree-like structure
         planetdict = {'star1': 'Target1', 'planet1': 'Planet1'}
 
         input_dict = {'Target1': ticstar.to_pastis(flag_transit),
                       'Planet1': planet.to_pastis(flag_transit),
-                      'PlanSys1': planetdict}        
-        
+                      'PlanSys1': planetdict}
+
     elif scenario in ['BEB', 'beb']:
         beb_params = _draw_parameters_beb(ticstar, **kwargs)
-        
+
         # Flag non-transiting planets
         flag_eclipse = conservative_transit_flag(beb_params)
 
@@ -63,15 +64,15 @@ def draw_parameters(params, scenario, nsimu=1, **kwargs):
         binarydict = beb_params[-1].to_pastis(flag_eclipse)
         # Add tree-like structure
         binarydict.update({'star1': 'Blend1', 'star2': 'Blend2'})
-        
-        #binarydict['P'] = beb_params[1].period
-        
+
+        # binarydict['P'] = beb_params[1].period
+
         input_dict = {'Target1': ticstar.to_pastis(flag_eclipse),
                       'Blend1': beb_params[0].to_pastis(flag_eclipse),
                       'Blend2': beb_params[1].to_pastis(flag_eclipse),
                       'IsoBinary1': binarydict
                       }
-    
+
     return input_dict
 
 
@@ -82,54 +83,54 @@ def _draw_parameters_pla(ticstar, **kwargs):
     planet = c.PlanetParameters(minradius=minimum_radius)
     # Draw parameters
     planet.draw(len(ticstar))
-        
+
     return planet
-    
+
 
 def _draw_parameters_beb(ticstar, **kwargs):
     """
     Draw parameters for the BEB scenario.
-    
-    This is done by drawing a background star and build the binary, much like 
+
+    This is done by drawing a background star and build the binary, much like
     the pastis object builder.
     """
     # Build primary
-    bkg_primary = c.BackgroundStarParameters()
+    bkg_primary = c.BackgroundStarParameters(minmass=0.5, maxdist=5)
     # Draw parameters for primary
     bkg_primary.draw(len(ticstar))
-    
+
     # Build secondary
     bkg_secondary = c.SecondaryStarParameters(bkg_primary)
     bkg_secondary.draw()
-    
+
     # Draw orbit
     orbit = c.OrbitParameters(orbittype='binary')
     orbit.draw(len(ticstar))
-    
+
     return [bkg_primary, bkg_secondary, orbit]
-    
+
 
 def conservative_transit_flag(params):
     """
     Flag systems according to whether they transit or not.
-    
+
     Use a conservative approach when selecting stellar masses and radii.
     The objective at this point is performing a cut without having to buld the
     actual pastis objects.
-    
+
     :param (Parameter class) params: instance of the parameter Class
     """
     # Check which class input belongs to.
     # Target + planet
-    if (isinstance(params[0], c.TargetStarParameters) and 
-        isinstance(params[1], c.PlanetParameters)):
+    if (isinstance(params[0], c.TargetStarParameters) and
+    isinstance(params[1], c.PlanetParameters)):
             #do something planety
             print('Checking parameters for planetary system')
-            
+
             # Get masses
             mass2 = params[1].mass_mearth * cts.GMearth / cts.GMsun
             radius2_au = params[1].radius_rearth * cts.Rearth / cts.au
-            
+
             # To be concervative, choose the smallest reasonable mass
             # and the largest possible radius
             # This will make the planet orbit closer to a larger star
@@ -138,24 +139,24 @@ def conservative_transit_flag(params):
 
             # Define object containing orbital parameters
             orbit_params = params[1]
-            
+
     # background star + secondary
-    elif (isinstance(params[0], c.BackgroundStarParameters) and 
+    elif (isinstance(params[0], c.BackgroundStarParameters) and
           isinstance(params[1], c.SecondaryStarParameters)):
             # do something BEB
             print('Checking parameters for BEB system')
-            
+
             assert len(params) > 2, "Missing parameter object for the orbit"
-            
+
             # Get masses
             mass1 = params[0].mass
             mass2 = params[1].mass
-            
+
             # Get radii
             # Again, to be conservative, choose LARGE radius
             radius1_au = 10.0 * cts.Rsun / cts.au
             radius2_au = 10.0 * cts.Rsun / cts.au
-            
+
             # Define object containing orbital parameters
             orbit_params = params[2]
 
@@ -171,6 +172,6 @@ def conservative_transit_flag(params):
 
     # compute impact parameter
     b = r0 * np.cos(incl_rad)
-         
+
     # Return condition of transit
     return b <= 1 + radius2_au/radius1_au
